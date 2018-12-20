586888d323504e7f94e2cc12739259ff14c881da
apollo0725 <superapollo0725@gmail.com>
2018-12-19 23:26:10 -0800

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://frost0625:andrew123456@ds245082.mlab.com:45082/frost0625",
+  secretOrKey: "secret"
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+
+
+// DB Config
+const db = require("./Config/keys").mongoURI;
+
+
+// Connect to MongoDB
+mongoose
+  .connect(db,{ useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.json()); 
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Use routes
+app.use('/api/users',users);
+app.use('/api/profile',profile);
+app.use('/api/posts',posts);
+
+const port = process.env.PORT || 5050;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
