74d07f7a477a5a527ff5e272b1ea476c55440c5e
apollo0725 <superapollo0725@gmail.com>
2018-12-20 05:24:58 -0800

    Validation-Handlers2

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,8 @@ const passport = require('passport');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
 
 // Load User model
 const User = require("../../models/User");
@@ -67,6 +69,13 @@ router.post("/register", (req, res) => {
 // @desc  Login User / Returning JWT Token
 // @acess Public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+  // Check Vlidation
+  if(!isValid){
+    return res.status(400).json(errors);
+  }
+
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -75,7 +84,8 @@ router.post('/login', (req, res) => {
     .then(user => {
       // Check for user
       if(!user){
-        return res.status(404).json({email: 'User not found'});
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
       }
 
       // Check Password
@@ -97,7 +107,8 @@ router.post('/login', (req, res) => {
                 });
               });
           } else {
-            return res.status(400).json({password: 'Password incorrect'});
+            errors.password = 'Password incorrect';
+            return res.status(400).json(errors);
           }
         });
     });
